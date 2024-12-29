005bbd2bdab0ce5d1fb8e153dda9502d3f653e62
Kolapo Wariz <kolapowariz@gmail.com>
2024-12-29 01:01:50 +0100

    Setting up dashboard page

--- src/components/dashboard/NavLinks.tsx
+++ src/components/dashboard/NavLinks.tsx
@@ -1,7 +1,7 @@
 'use client';
 import {
   DocumentDuplicateIcon,
-  HomeIcon
+  HomeIcon, MusicalNoteIcon
 } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
@@ -10,17 +10,17 @@ import { usePathname } from 'next/navigation';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'Home', href: '/', icon: HomeIcon },
   {
-    name: 'Editor',
-    href: '/dashboard/editor',
+    name: 'Files',
+    href: '/',
     icon: DocumentDuplicateIcon,
   },
-  // {
-  //   name: 'My Posts',
-  //   href: '/dashboard/userPosts',
-  //   icon: UserGroupIcon,
-  // }
+  {
+    name: 'Podcast',
+    href: '/',
+    icon: MusicalNoteIcon,
+  }
 ];
 
 export default function NavLinks() {
@@ -33,7 +33,7 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx("flex grow items-center justify-center gap-2 rounded-md bg-gray-50 dark:text-black p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex md:grow-0 md:justify-start md:p-2 md:px-3", {
+            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3", {
               'bg-sky-100 text-blue-600': pathname === link.href,
             })}
           >
